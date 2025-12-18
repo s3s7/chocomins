526607e673acdcd830061d20aaa48c22de243f2f
shun <samesamejinbei@gmail.com>
2025-12-18 21:55:36 +0900

    add:ブランド追加機能作成中

---
+++ web/src/app/brands/_components/brand-list.tsx
@@ -0,0 +1,21 @@
+import { Brand } from '@prisma/client'
+import { BrandItem } from './brand-item'
+
+type BrandListProps = {
+  brands: Brand[]
+  currentUserRole: string
+}
+
+export function BrandList({ brands, currentUserRole }: BrandListProps) {
+  if (brands.length === 0) {
+    return <p className="text-gray-500">ブランドはまだありません。</p>
+  }
+
+  return (
+    <ul className="space-y-4">
+      {brands.map((brand) => (
+        <BrandItem key={brand.id} brand={brand} currentUserRole={currentUserRole} />
+      ))}
+    </ul>
+  )
+}
