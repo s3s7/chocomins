83d231d5bdf343ac74099756fae85d1ec96c69a8
shun <samesamejinbei@gmail.com>
2025-12-20 10:55:10 +0900

    add:ブランド追加機能作成中

---
+++ web/src/app/actions/brand/create.ts
@@ -0,0 +1,51 @@
+'use server'
+
+import { auth } from '@/lib/auth'
+import { revalidatePath } from 'next/cache'
+import { BrandInput, brandSchema } from '@/schemas/brand'
+import { ActionResult, ErrorCodes } from '@/types'
+import { createBrandInDB } from '../../../services/create-brand'
+
+export async function createBrand(
+  _: ActionResult | null,
+  formData: FormData,
+): Promise<ActionResult> {
+  // 認証情報を取得
+  const session = await auth()
+
+  // ユーザーがログインしていない場合
+  if (!session?.user) {
+    return { isSuccess: false, errorCode: ErrorCodes.UNAUTHORIZED }
+  }
+
+  const rawCountry = formData.get('country')
+  // フォームデータから投稿情報を取得
+  const input: BrandInput = {
+    name: formData.get('name')?.toString() ?? '',
+    country: rawCountry ? rawCountry.toString() : undefined,
+  }
+
+  // バリデーションチェック
+  const parsed = brandSchema.safeParse(input)
+  if (!parsed.success) {
+    // 入力が不正な場合
+    return { isSuccess: false, errorCode: ErrorCodes.INVALID_INPUT }
+  }
+
+  try {
+    // データベースに保存
+    await createBrandInDB({
+      ...parsed.data,
+    })
+
+    // 一覧ページなどを再検証
+    revalidatePath('/brands')
+
+    // 成功時
+    return { isSuccess: true }
+  } catch (err) {
+    // サーバーエラーの場合
+    console.error('error:', err)
+    return { isSuccess: false, errorCode: ErrorCodes.SERVER_ERROR }
+  }
+}
---
+++ web/src/app/actions/brand/delete.ts
@@ -0,0 +1,51 @@
+'use server'
+
+import { auth } from '@/lib/auth'
+import { revalidatePath } from 'next/cache'
+import { deleteBrandFromDB } from '../../../services/delete-brand'
+import { ActionResult, ErrorCodes } from '@/types'
+import {
+  DeleteBrandInput,
+  deleteBrandSchema,
+} from '@/schemas/brand'
+
+export async function deleteBrand(
+  formData: FormData,
+): Promise<ActionResult> {
+  const session = await auth()
+  if (!session?.user) {
+    return { isSuccess: false, errorCode: ErrorCodes.UNAUTHORIZED }
+  }
+  const input: DeleteBrandInput = {
+    brandId: formData.get('brandId')?.toString() ?? '',
+  }
+
+  const parsed = deleteBrandSchema.safeParse(input)
+  if (!parsed.success) {
+    return { isSuccess: false, errorCode: ErrorCodes.INVALID_INPUT }
+  }
+
+  try {
+    await deleteBrandFromDB({
+      ...parsed.data,
+    })
+
+    revalidatePath('/brands')
+    return { isSuccess: true }
+  } catch (err) {
+    console.error('deleteBrand error:', err)
+
+    if (err instanceof Error) {
+      switch (err.message) {
+        case ErrorCodes.NOT_FOUND:
+          return { isSuccess: false, errorCode: ErrorCodes.NOT_FOUND }
+        case ErrorCodes.UNAUTHORIZED:
+          return { isSuccess: false, errorCode: ErrorCodes.FORBIDDEN }
+        default:
+          return { isSuccess: false, errorCode: ErrorCodes.SERVER_ERROR }
+      }
+    }
+
+    return { isSuccess: false, errorCode: ErrorCodes.SERVER_ERROR }
+  }
+}
