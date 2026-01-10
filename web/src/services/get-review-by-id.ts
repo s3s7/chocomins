d7154e51e94e35db2892b90ead069b1dc93f35ba
shun <samesamejinbei@gmail.com>
2026-01-11 08:30:53 +0900

    add:map表示のためのPlaceモデルの記述、表示用に数値への変換処理を追加

--- web/src/services/get-review-by-id.ts
+++ web/src/services/get-review-by-id.ts
@@ -1,13 +1,29 @@
 import { prisma } from '@/lib/prisma'
 
 export async function getReviewById(reviewId: string) {
-  return await prisma.review.findUnique({
+  const review = await prisma.review.findUnique({
     where: { id: reviewId },
     include: {
       user: true,
       chocolate: {
         select: { name: true },
       },
+      place: {
+        select: { lat: true, lng: true },
+      },
     },
   })
+
+  if (!review) return null
+
+  return {
+    ...review,
+    place: review.place
+      ? {
+          ...review.place,
+          lat: review.place.lat?.toNumber() ?? null,
+          lng: review.place.lng?.toNumber() ?? null,
+        }
+      : null,
+  }
 }
